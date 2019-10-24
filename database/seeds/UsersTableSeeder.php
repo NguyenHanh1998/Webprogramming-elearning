c87e01b6041f158e8870a9dc65429348850aeb4d
Nguyen Kien <kiennd010498@gmail.com>
2019-10-24 15:11:14 +0700

    add: users database seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'role' => 0,
+            'password' => bcrypt('123456')
+        ]);
+
+        factory(App\Models\User::class, 40)->create();
+    }
+}
