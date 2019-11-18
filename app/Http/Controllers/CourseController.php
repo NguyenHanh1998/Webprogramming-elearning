d7cc67dd79d27ba95195d800927442f980bb41eb
Nguyen Kien <kiennd010498@gmail.com>
2019-11-18 15:22:50 +0700

    add: update course

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -38,6 +38,23 @@ class CourseController extends Controller
         }
     }
 
+    public function show(Request $request, $id)
+    {
+        try {
+            $course = $this->_courseRepository->get($id);
+            return response()->json([
+                'data' => $course
+            ], Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Error on fetching',
+                    'internal_message' => $e->getMessage()
+                ]
+            ]);
+        }
+    }
+
     public function store(Request $request, $user_id)
     {
         $params = $request->only(
@@ -60,6 +77,29 @@ class CourseController extends Controller
         }
     }
 
+    public function update(Request $request, $user_id, $id)
+    {
+        if (!$this->validIdentity(Auth::id(), $user_id))
+            return $this->error('Unauthorized');
+        $updateParams = $request->only(
+            'name',
+            'fee',
+            'description', 
+            'requirement', 
+            'learnable'
+        );
+        $course = $this->_courseRepository->get($id);
+        // return response()->json(['data' => $updateParams]);
+        try {
+            $course->update($updateParams);       
+            return response()->json(['data' => 'Update success'], Response::HTTP_NO_CONTENT);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Update error' . $e->getMessage(),
+            ], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
     public function enrolledStudent(Request $request, $user_id, $id)
     {
         return response()->json(['data' => [
@@ -70,10 +110,11 @@ class CourseController extends Controller
     public function userCourses(Request $request, $user_id)
     {
         $tokenId = Auth::id();
-        if(!$this->validIdentity($tokenId, $user_id))
+        if (!$this->validIdentity($tokenId, $user_id))
             return $this->error('Unauthorized');
         $courses = $this->_userRepository->getMyCourseList($user_id);
-        return response()->json(['data' => [
+        return response()->json([
+            'data' => [
                 'courses' => $courses,
             ]
         ]);
